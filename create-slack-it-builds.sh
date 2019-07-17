7e8c678c627e6cc1694846ae947c4393b4759a2e
William PC <w_calandrini@hotmail.com>
2019-07-17 21:33:49 +0200

    Add files via upload

--- create-slack-it-builds.sh
+++ create-slack-it-builds.sh
@@ -1,42 +1,66 @@
 
+CTNAME=Slackware
 LOGPATH=~/log/lxc-slack-it
-RELEASE="13.1 13.37 14.1 14.2"
+ARCHS="x86 x86_64"
+RELEASE="13.37 14.0 14.1 14.2"
 LXCPATH=/var/lib/lxc/slack-it/slackbuilds
 
 [ ! -d $LOGPATH ] && mkdir -p $LOGPATH
 
-for rv in $RELEASE; do
-  echo "Creating container Slackware-$rv"
-  arch=i486 release=$rv lxc-create -P $LXCPATH -t slackware -n Slackware-$rv > $LOGPATH/lxc-slackware-$rv\_deploy.log
-  arch=x86_64 release=$rv lxc-create -P $LXCPATH -t slackware -n Slackware64-$rv > $LOGPATH/lxc-slackware64-rv\_deploy.log
-done
+function deploy(){
+  echo "-> Creating container $1"
+  arch=$ARCH release=$rv lxc-create -P $LXCPATH -t slackware -n $1 > $LOGPATH/lxc-$(echo $1 | tr [:upper:] [:lower:])\_deploy.log
+}
 
-sleep 2; lxc-ls -P $LXCPATH -f; sleep 2
-
-echo "Configuring containers..."
-for rv in $RELEASE; do
+function configure(){
+  SLACKPKGCFG=/etc/slackpkg/slackpkg.conf
   macaddr=$(echo $(date)|md5sum|sed 's/^\(..\)\(..\)\(..\)\(..\)\(..\).*$/02:\1:\2:\3:\4:\5/')
 
+  grep "lxc.network." $LXCPATH/$1/config
+  if [ $? == "1" ]; then
 echo "lxc.network.0.type = veth
 lxc.network.0.flags = up
 lxc.network.0.name = eth0-nat
 lxc.network.0.link = virbr0
-lxc.network.0.hwaddr = $macaddr" >> $LXCPATH/Slackware-$rv/config
-  sed -i 's/BATCH=off/BATCH=on/' $LXCPATH/Slackware-$rv/rootfs/etc/slackpkg/slackpkg.conf
-  sed -i 's/DEFAULT_ANSWER=n/DEFAULT_ANSWER=y/' $LXCPATH/Slackware-$rv/rootfs/etc/slackpkg/slackpkg.conf 
+lxc.network.0.hwaddr = $macaddr" >> $LXCPATH/$1/config
+  fi
+  sed -i 's/BATCH=off/BATCH=on/' $LXCPATH/$1/rootfs/$SLACKPKGCFG
+  sed -i 's/DEFAULT_ANSWER=n/DEFAULT_ANSWER=y/' $LXCPATH/$1/rootfs/$SLACKPKGCFG
 
-  sed -i 's/BATCH=off/BATCH=on/' $LXCPATH/Slackware64-$rv/rootfs/etc/slackpkg/slackpkg.conf
-  sed -i 's/DEFAULT_ANSWER=n/DEFAULT_ANSWER=y/' $LXCPATH/Slackware64-$rv/rootfs/etc/slackpkg/slackpkg.conf  
   sleep 1
-done
+}
+
+function finstall(){
+  LOGFILE=lxc-$(echo $1 | tr [:upper:] [:lower:])\_install.log
+  echo "Starting container $1"
+  lxc-start -P $LXCPATH -n $1; sleep 2
+  lxc-attach -P $LXCPATH -n $1 -- dhcpcd eth0-nat
+  lxc-attach -P $LXCPATH -n $1 -- slackpkg update > $LOGPATH/$LOGFILE
+  lxc-attach -P $LXCPATH -n $1 -- slackpkg install slackware$SLACKNAME >> $LOGPATH/$LOGFILE
+}
 
 
+function ctmanage(){
 for rv in $RELEASE; do
-  echo "Installing full package series"
-  lxc-start -P $LXCPATH -n Slackware-$rv
-  lxc-attach -P $LXCPATH -n Slackware-$rv -- slackpkg install slackware
-  lxc-start -P $LXCPATH -n Slackware64-$rv
-  lxc-attach -P $LXCPATH -n Slackware64-$rv -- slackpkg install slackware64
-done
+  for arch in $ARCHS; do
+    case "$arch" in
+      x86) ARCH=i486; SLACKNAME="" ;;
+      x86_64) ARCH=x86_64; SLACKNAME="64" ;;
+       *) echo "Unkown arch"; exit;;
+    esac    
+    $1 $CTNAME$SLACKNAME-$rv
+   done
+  done
+}
 
+echo "Deploy Slackware containers"
+ctmanage deploy
+
+echo "Configuring containers"
+ctmanage configure
+sleep 1; lxc-ls -P $LXCPATH -f; sleep 3
+
+echo "Install full package series"
+ctmanage finstall
+sleep 1; lxc-ls -P $LXCPATH -f; sleep 3
 
