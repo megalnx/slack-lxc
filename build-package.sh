9a2cb3e8efb1f7b6878fb29256775deb78ee4a11
William PC <w_calandrini@hotmail.com>
2019-07-22 22:41:49 +0200

    Add files via upload

--- build-package.sh
+++ build-package.sh
@@ -1,27 +1,43 @@
+
 CTNAME=Slackware
-ARCHS="x86 x86_64"
-RELEASE="13.37 14.0 14.1 14.2"
+ARCHS=${ARCHS:-"x86 x86_64"}
+RELEASE=${RELEASE:-"13.37 14.0 14.1 14.2"}
 LXCPATH=/var/lib/lxc/slack-it/slackbuilds
 
 
+function ver { printf "%03d%03d%03d%03d" $(echo "$1" | tr '.' ' '); }
 
 function slackit_build(){
-  lxc-start -P $LXCPATH -n $1
-  lxc-attach -P $LXCPATH -n $1 -- slackpkg update
-  lxc-attach -P $LXCPATH -n $1 -- slackbuild-management.sh pack $2
+  echo "Starting $CTNAME$SLACKNAME-$rv";
+  lxc-start -P $LXCPATH -n $1; sleep 10
+#  lxc-attach -P $LXCPATH -n $1 -- slackpkg update
+#  lxc-attach -P $LXCPATH -n $1 -- slackpkg upgrade-all
+  lxc-attach -P $LXCPATH -n $1 --set-var ARCH=$ARCH --set-var SBo_OUTPUT=/tmp -- slackbuild-management.sh ${@:2}
+  echo "Stoping $CTNAME$SLACKNAME-$rv";
+  lxc-stop -P $LXCPATH -n $1
 }
 
 function ctmanage(){
 for rv in $RELEASE; do
   for arch in $ARCHS; do
     case "$arch" in
-      x86) ARCH=i486; SLACKNAME="" ;;
+
+      x86)
+      if [ $(ver $rv) -lt $(ver 14.2) ]; then
+         ARCH=i486; SLACKNAME="" 
+      else
+         ARCH=i586; SLACKNAME="" 
+      fi
+       ;;
       x86_64) ARCH=x86_64; SLACKNAME="64" ;;
        *) echo "Unkown arch"; exit;;
     esac    
-    slackit_build $CTNAME$SLACKNAME-$rv $2
+    echo "slackbuild-management.sh $@ at $CTNAME$SLACKNAME-$rv"; sleep 2
+    slackit_build $CTNAME$SLACKNAME-$rv $@
    done
   done
 }
 
-ctmanage $2
+ctmanage $@
+
+
